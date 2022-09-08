18c9da2ff7bde7028849e5fe3bd9384e1afd6626
RayaniGomes <rayanigomesrn100@gmail.com>
2022-09-08 01:45:32 -0300

    atualizar

--- js/modal-card.js
+++ js/modal-card.js
@@ -13,20 +13,3 @@ function onCancel(){
 function onConfirm(){
   onCancel();
 }
-
-
-function onAdd(){
-  let confirmation= document.getElementById("confirmation2");
-  if(!confirmation.classList.contains("modal-open")){
-    confirmation.classList.add("modal-open");
-  }
-}
-
-function onCancel(){
-  let confirmation= document.getElementById("confirmation2");
-    confirmation.classList.remove("modal-open");
-}
-
-function onConfirm(){
-  onCancel();
-}
