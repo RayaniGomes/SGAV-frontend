c709bd15da5d4189da89e6975016c976cbb6038a
RayaniGomes <rayanigomesrn100@gmail.com>
2022-09-08 01:18:22 -0300

    atualizar

--- js/modal-card.js
+++ js/modal-card.js
@@ -1,13 +1,32 @@
 function onAdd(){
-    let confirmation= document.getElementById("confirmation");
-    if(!confirmation.classList.contains("modal-open")){
-        confirmation.classList.add("modal-open");
-    }
+  let confirmation= document.getElementById("confirmation");
+  if(!confirmation.classList.contains("modal-open")){
+    confirmation.classList.add("modal-open");
   }
-  function onCancel(){
-    let confirmation= document.getElementById("confirmation");
+}
+
+function onCancel(){
+  let confirmation= document.getElementById("confirmation");
     confirmation.classList.remove("modal-open");
+}
+
+function onConfirm(){
+  onCancel();
+}
+
+
+function onAdd(){
+  let confirmation= document.getElementById("confirmation2");
+  if(!confirmation.classList.contains("modal-open")){
+    confirmation.classList.add("modal-open");
   }
-  function onConfirm(){
-      onCancel();
-  }
+}
+
+function onCancel(){
+  let confirmation= document.getElementById("confirmation2");
+    confirmation.classList.remove("modal-open");
+}
+
+function onConfirm(){
+  onCancel();
+}
